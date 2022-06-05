14f2023297bdb53234b969b287b3396f8bd544d6
盧露 <low.lucyy@gmail.com>
2022-06-04 23:44:12 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mobius ai credit risk assessment',
+    author='mobuis',
+    license='MIT',
+)
